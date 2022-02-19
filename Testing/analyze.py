819e8805944ce8b0ea64e9b46568592f38354a05
grahamwwilson <gwwilson@ku.edu>
2022-02-19 15:13:16 -0600

    Add t-statistic confidence intervals

--- Testing/analyze.py
+++ Testing/analyze.py
@@ -42,9 +42,13 @@ def Analyze(x,y,z):
     print('x:',x)
     print('y:',y)
     print('z:',z)
-    print('x, N:',len(x),'mean:',st.mean(x),' +- ',math.sqrt(st.variance(x)/len(x)),'median:',st.median(x),'var:',st.variance(x))
-    print('y, N:',len(y),'mean:',st.mean(y),' +- ',math.sqrt(st.variance(y)/len(y)),'median:',st.median(y),'var:',st.variance(y))
-    print('z, N:',len(z),'mean:',st.mean(z),' +- ',math.sqrt(st.variance(z)/len(z)),'median:',st.median(z),'var:',st.variance(z))
+# Round these a little when printing   
+    print('x, nx:',len(x),'mean:',round(st.mean(x),6),' +- ',round(math.sqrt(st.variance(x)/len(x)),6),
+          'median:',round(st.median(x),6),'sx:',round(math.sqrt(st.variance(x)),6))
+    print('y, ny:',len(y),'mean:',round(st.mean(y),6),' +- ',round(math.sqrt(st.variance(y)/len(y)),6),
+          'median:',round(st.median(y),6),'sy:',round(math.sqrt(st.variance(y)),6))          
+    print('z,  N:',len(z),'mean:',round(st.mean(z),6),' +- ',round(math.sqrt(st.variance(z)/len(z)),6),
+          'median:',round(st.median(z),6),'sz:',round(math.sqrt(st.variance(z)),6))
     zcopy = z.copy()
     zcopy.sort()
     print('z sorted ',zcopy)
@@ -80,14 +84,38 @@ def Analyze(x,y,z):
 # They differ for sure when nx != ny. 
 
     spB = math.sqrt( ((nx-1)*st.variance(x) + (ny-1)*st.variance(y))/(nx+ny-2) )
+    pooledsE = spB*math.sqrt((1/nx) + (1/ny))   # on the difference
     sDelta = math.sqrt( ( st.variance(x)/nx ) + ( st.variance(y)/ny ) )
 
 # Equal variance version (Student's t-test)
-    t1 = (st.mean(x) - st.mean(y)) / (spB*math.sqrt((1/nx) + (1/ny)))
-    print('nx,ny,spB,Student t1:',nx,ny,spB,t1)
+    t1 = (st.mean(x) - st.mean(y)) / pooledsE
+    print(' ')
+    print('nx,ny,spB,pooledsE,Student t1:',nx,ny,spB,pooledsE,t1)
+    
+# Maybe use code like
+# https://stats.stackexchange.com/questions/475289/confidence-interval-for-2-sample-t-test-with-scipy 
+# for confidence intervals
 
+    pt = 2.0*stats.t.cdf(-abs(t1), nx+ny-2)
+    print('2-sided equal variance t-test p-value ',pt,' (dof = ',nx+ny-2,')')
+    
+    lbound1 = (st.mean(x) - st.mean(y)) - stats.t.ppf(0.975, nx+ny-2)*pooledsE
+    ubound1 = (st.mean(x) - st.mean(y)) + stats.t.ppf(0.975, nx+ny-2)*pooledsE
+    print('95% CL interval on x - y : [',lbound1,',',ubound1,']')
+    
 # Unequal variance version (Welch's t-test)
     t2 = (st.mean(x) - st.mean(y)) / sDelta
+    print(' ')
     print('nx,ny,sDelta,Welch t2:',nx,ny,sDelta,t2)
+    vx = st.variance(x)
+    vy = st.variance(y)
+    df = ( (vx / nx) + (vy / ny))**2 / ( (vx**2 / (nx**2 * (nx - 1)) + vy**2 / (ny**2 * (ny - 1)))) 
+    ptw = 2.0*stats.t.cdf(-abs(t2), df)
+    print('2-sided non-equal variance t-test p-value ',ptw,' (dof =',df,')')
+    lbound2 = (st.mean(x) - st.mean(y)) - stats.t.ppf(0.975, df)*sDelta
+    ubound2 = (st.mean(x) - st.mean(y)) + stats.t.ppf(0.975, df)*sDelta
+    print('95% CL interval on x - y : [',lbound2,',',ubound2,']')    
+    
+# Note I have checked these against an R implementation, they seem to be sound.          
     
     return T1Obs, T2Obs, T3Obs
--- Testing/myPythonCheck.py
+++ Testing/myPythonCheck.py
@@ -4,3 +4,5 @@ import sys
 def Check():
     if sys.version_info[0] < 3:
         raise Exception("Python 3 or a more recent version is required for proper execution")
+    else:
+        print("Python 3 check passed")
